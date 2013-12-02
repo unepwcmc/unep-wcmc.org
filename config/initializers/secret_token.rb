902ebda5691671da7c8bf0929e0f0aa8c7d2b469
kustosz <marcin.kostrzewa@llp.pl>
2013-12-02 14:48:18 +0100

    move mockups to public

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnepWcmcOrg::Application.config.secret_key_base = '30284980334c7c237daf0567608698099f4eb674f68a638803abcaea1e8b19c8686194325f4d8c2c3b653ee901322f3995400782f6445088f6298798b385e69b'
