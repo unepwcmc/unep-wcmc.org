cd48eb9c5f13aebab90cce1148caee04bd7bf069
kustosz <marcin.kostrzewa@llp.pl>
2013-12-10 15:55:07 +0100

    add filtering by content type

--- app/assets/javascripts/datasets_resources/datasets_ctrl.js
+++ app/assets/javascripts/datasets_resources/datasets_ctrl.js
@@ -6,9 +6,34 @@ angular.module("DatasetsResources").controller("DatasetsCtrl", ["$scope", functi
     });
   }
 
+  var selectAll = function (contentTypes) {
+    _.each(contentTypes, function (contentType) {
+      contentType.selected = true;
+    });
+    applyFilters();
+  }
+
+  var applyFilters = function () {
+    $scope.activeDatasets = [];
+    _.each($scope.datasets, function (dataset) {
+      var contentType = _.findWhere($scope.contentTypes, {singular: dataset.content_type});
+      if (contentType.selected) {
+        $scope.activeDatasets.push(dataset);
+      }
+    });
+  }
+
   $scope.init = function (data) {
+    $scope.activeDatasets = data.datasets;
     $scope.datasets = data.datasets;
     $scope.contentTypes = data.content_types;
     countByContentType($scope.datasets, $scope.contentTypes);
+    selectAll($scope.contentTypes);
   }
+
+  $scope.toggleContentType = function () {
+    this.contentType.selected = !this.contentType.selected;
+    applyFilters();
+  }
+
 }]);
