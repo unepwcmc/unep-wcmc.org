06f77078d14ad3264f091c35b1bcd7da78a324aa
kustosz <marcin.kostrzewa@llp.pl>
2013-12-27 13:32:14 +0100

    fix db schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,24 +16,6 @@ ActiveRecord::Schema.define(version: 20131223144520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-
   create_table "cms_blocks", force: true do |t|
     t.integer  "page_id",    null: false
     t.string   "identifier", null: false
