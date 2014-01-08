fad331f3b688951a2457dbcc73beab94869ce447
kustosz <marcin.kostrzewa@llp.pl>
2014-01-08 13:26:55 +0100

    add a spec for submissions controller

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe SubmissionsController do
-  before do
-    @form = create(:form)
-    create(:field, form_id: @form.id, type: "TextField")
-    create(:field, form_id: @form.id, type: "FileField")
-  end
-
   describe "#new" do
+    before do
+      @form = create(:form)
+      create(:field, form_id: @form.id, type: "TextField")
+      create(:field, form_id: @form.id, type: "FileField")
+    end
+
     it "builds new form submission" do
       get :new, form_id: @form.id
       assigns(:submission).should_not be_blank
@@ -16,6 +16,22 @@ describe SubmissionsController do
     it "sets all submission's fields" do
       get :new, form_id: @form.id
       assigns(:submission).field_submissions.size.should == 2
+    end
+  end
+
+  describe "#edit" do
+    before do
+      @form = create(:form)
+      create(:field, form_id: @form.id, type: "TextField")
+      create(:field, form_id: @form.id, type: "FileField")
+      @submission = Submission.build_for_form(@form)
+      @submission.save
+    end
+
+    it "assigns form and submission" do
+      get :edit, form_id: @form.id, id: @submission.id
+      assigns(:submission).should == @submission
+      assigns(:form).should == @form
     end
   end
 end
