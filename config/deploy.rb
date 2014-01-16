c75fa7641fecaaab059452ee56f1c0e76f8c5f85
Stuart <stuart@stuart-SVS1513C5E.(none)>
2014-01-16 14:37:32 +0000

    Changes made for EC2 deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,26 @@
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+
+
 set :application, "unepwcmc"
 set :repository, "git@github.com:unepwcmc/unep-wcmc.org"
 set :deploy_via, :remote_cache
 set :scm, :git
 set :branch, "rails"
+set :scm_username, "unepwcmc-read"
 
-set :ssh_options, {forward_agent: true}
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
-set :domain, "unep-wcmc.demo.llp.pl"
-set :port, 20120
+set :domain, "ec2-54-195-244-198.eu-west-1.compute.amazonaws.com"
 set :rails_env, "production"
 
-role :web, domain
-role :app, domain
-role :db, domain, :primary => true
+server "ec2-54-195-244-198.eu-west-1.compute.amazonaws.com", :app, :web, :db, :primary => true
 
-set :user, "unepwcmc"
+set :user, "ubuntu"
 set :use_sudo, false
-set :deploy_to, "/home/app/unepwcmc"
+set :deploy_to, "/home/ubuntu/unepwcmc"
 
 set :keep_releases, 2
 
@@ -30,17 +34,44 @@ namespace :db do
   end
 end
 
-set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+# set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
-namespace :deploy do
-  task :start, roles: :app, except: { no_release: true } do
-    run "cd #{current_path} && #{try_sudo} bundle exec unicorn -p 3000 -E #{rails_env} -D -P #{unicorn_pid}"
-  end
-  task :stop, roles: :app, except: { no_release: true } do
-    run "#{try_sudo} kill `cat #{unicorn_pid}`"
-  end
-  task :restart, roles: :app, except: { no_release: true } do
-    stop
-    start
+#namespace :deploy do
+#  task :start, roles: :app, except: { no_release: true } do
+#    run "cd #{current_path} && #{try_sudo} bundle exec unicorn -p 3000 -E #{rails_env} -D -P #{unicorn_pid}"
+#  end
+#  task :stop, roles: :app, except: { no_release: true } do
+#    run "#{try_sudo} kill `cat #{unicorn_pid}`"
+#  end
+#  task :restart, roles: :app, except: { no_release: true } do
+#    stop
+#    start
+#  end
+#end
+
+
+namespace :db do
+  task :setup do
+    the_host = Capistrano::CLI.ui.ask("Database IP address: ")
+    database_name = Capistrano::CLI.ui.ask("Database name: ")
+    database_user = Capistrano::CLI.ui.ask("Database username: ")
+    pg_password = Capistrano::CLI.password_prompt("Database user password: ")
+
+    require 'yaml'
+
+    spec = {
+      "#{rails_env}" => {
+        "adapter" => "postgresql",
+        "database" => database_name,
+        "username" => database_user,
+        "host" => the_host,
+        "password" => pg_password
+      }
+    }
+
+    run "mkdir -p #{shared_path}/config"
+    put(spec.to_yaml, "#{shared_path}/config/database.yml")
   end
 end
+after "deploy:setup", 'db:setup'
+
