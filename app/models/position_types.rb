bd62177da234b368f18cee972f267806351a5e20
katszwn <katarzyna.szawan@lunarlogic.io>
2014-01-20 14:53:50 +0100

    Fix position type model, add seeds

--- app/models/position_types.rb
+++
@@ -1,2 +0,0 @@
-class TypesOfPosition < ActiveRecord::Base 
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,12 @@ if ContentType.count == 0
     ContentType.create(singular: type[0], plural: type[1])
    end
 end
+
+if PositionType.count == 0
+   ["permanent",
+    "temporary",
+    "part-time",
+    "full-time" ].each do |position|
+      PositionType.create(name: position)
+    end
+end
