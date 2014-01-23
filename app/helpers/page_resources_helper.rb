4ce1fc0391e8b3bf5448462715089cd6cc6bc1c9
kustosz <marcin.kostrzewa@llp.pl>
2014-01-23 15:26:21 +0100

    use normalized date format on news item page

--- app/helpers/page_resources_helper.rb
+++ app/helpers/page_resources_helper.rb
@@ -1,5 +1,6 @@
 module PageResourcesHelper
-  def formatted_time(time_string)
+  def formatted_time(time_string=nil)
+    time_string ||= cms_page_content(:time)
     Time.parse(time_string).strftime("%d %B %Y")
   end
 end
