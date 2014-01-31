984e802d14cded04e0984bbbf1bdbf93b772be8f
kustosz <marcin.kostrzewa@llp.pl>
2014-01-31 15:47:27 +0100

    fixes for vacancy page

--- app/helpers/page_resources_helper.rb
+++ app/helpers/page_resources_helper.rb
@@ -3,4 +3,8 @@ module PageResourcesHelper
     time_string ||= cms_page_content(:time)
     Time.parse(time_string).strftime("%d %B %Y")
   end
+
+  def about_us_page
+    @about_us_page ||= Cms::Site.find_by_identifier('about-us').pages.root
+  end
 end
