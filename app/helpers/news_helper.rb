3dea963260f544797b8e7c9a11c366f0489d8147
katszwn <katarzyna.szawan@lunarlogic.io>
2014-02-03 15:03:18 +0100

    Tweak syntax

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,8 +1,8 @@
 module NewsHelper
 
   def related_news(current_news_id)
-    news_references = NewsReference.where('news_id' => current_news_id)
-    @related_news = Cms::Site.find_by_identifier('news').pages.root.children.published.where('id' => [news_references[0].related_news_id,  news_references[1].related_news_id])
+    news_references = NewsReference.where(news_id: current_news_id)
+    @related_news = Cms::Page.published.where("id IN (?)", news_references.map(&:related_news_id))
   end
 
 end
