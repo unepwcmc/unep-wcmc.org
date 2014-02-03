b3e205de454fa7dc6dd9d587d534d257033e895c
kustosz <marcin.kostrzewa@llp.pl>
2014-02-03 15:13:18 +0100

    restrict formatting options for CMS visual editor

--- app/assets/javascripts/comfortable_mexican_sofa/admin/tinymce_init.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/tinymce_init.js
@@ -1,7 +1,13 @@
 window.CMS.wysiwyg = function () {
   tinymce.init({
-    extended_valid_elements : "iframe[src|width|height|name|align]",
+    extended_valid_elements: "iframe[src|width|height|name|align]",
     plugins: "autolink link image autoresize media",
+    toolbar: "bold italic underline | styleselect | blockquote",
+    style_formats: [
+      {title: "Paragraph", block: "p"},
+      {title: "Header", block: "h3"}
+    ],
+    menubar: "edit insert",
     autoresize_min_height: 100,
     selector: "textarea[data-cms-rich-text]",
     height: 200
