25a33d4431eed8589b80a082a22b6e9412c8ab68
kustosz <marcin.kostrzewa@llp.pl>
2014-02-04 12:46:21 +0100

    tweak CMS visual editor toolbar options

--- app/assets/javascripts/comfortable_mexican_sofa/admin/tinymce_init.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/tinymce_init.js
@@ -2,10 +2,12 @@ window.CMS.wysiwyg = function () {
   tinymce.init({
     extended_valid_elements: "iframe[src|width|height|name|align]",
     plugins: "autolink link image autoresize media",
-    toolbar: "bold italic underline | styleselect | blockquote",
+    toolbar: "undo redo | bold italic underline | styleselect | blockquote | link image",
     style_formats: [
       {title: "Paragraph", block: "p"},
-      {title: "Header", block: "h3"}
+      {title: "Header 1", block: "h1"},
+      {title: "Header 2", block: "h2"},
+      {title: "Header 3", block: "h3"}
     ],
     menubar: "edit insert",
     autoresize_min_height: 100,
