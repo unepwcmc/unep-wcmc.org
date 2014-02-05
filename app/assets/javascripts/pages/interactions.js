021548dc6a457f3b7953f08725d431e440ed2e7d
kustosz <marcin.kostrzewa@llp.pl>
2014-02-05 19:02:20 +0100

    fix js interactions

--- app/assets/javascripts/pages/interactions.js
+++ app/assets/javascripts/pages/interactions.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
   $('button#trigger-menu').click(function() {
-    $('ul#button-menu').toggle('slow');
+    $('ul#button-menu').animate({height:"toggle"}, 500, function() {});
   });
+
   $('button#content-button').click(function() {
     $('ul#content-small').toggle('slow');
     $('button#content-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"});
   });
+
   $('button#fields-button').click(function() {
     $('ul#fields-small').toggle('slow');
     $('button#fields-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"})
   });
+
+  $('div.container.status#ok').slideDown('normal');
+  $('div.container.status#warning').slideDown('normal');
+
+  $('div.container.status#ok i.icon-chevron-up').click(function() {
+    $('div.container.status#ok').slideUp('normal');
+  });
+
+  $('div.container.status#warning i.icon-chevron-up').click(function() {
+    $('div.container.status#warning').slideUp('normal');
+  });
+
+  $('ul.subnav-small li.header#strengthen-button i').click(function() {
+    $('ul.expertise-small#strengthen').toggle('normal');
+  });
+
+  $('ul.subnav-small li.header#specialise-button i').click(function() {
+    $('ul.expertise-small#specialise').toggle('normal');
+  });
+
 });
