86764972593d860313330bad959c45a7024d1d9e
katszwn <katarzyna.szawan@lunarlogic.io>
2014-02-07 17:14:19 +0100

    Drop news_references table, remove model, helper and partial

--- app/helpers/news_helper.rb
+++
@@ -1,8 +0,0 @@
-module NewsHelper
-
-  def related_news(current_news_id)
-    news_references = NewsReference.where(news_id: current_news_id)
-    @related_news = Cms::Page.published.where("id IN (?)", news_references.map(&:related_news_id))
-  end
-
-end
