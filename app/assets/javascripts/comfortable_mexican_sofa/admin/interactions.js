286f65d842c483ee7958d19751d744776222c8a5
kustosz <marcin.kostrzewa@llp.pl>
2014-02-07 14:20:28 +0100

    change datepickers to datetime pickers

--- app/assets/javascripts/comfortable_mexican_sofa/admin/interactions.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/interactions.js
@@ -1,3 +1,11 @@
+window.CMS.timepicker = function () {
+  $('input[type=text][data-cms-datetime]').datetimepicker({
+    format: 'yyyy-mm-dd',
+    minView: 2,
+    autoclose: true
+  });
+}
+
 $(function () {
   $('input[value="twitter_share_text"]').parent().find("input[type=text]").attr({maxlength: 140});
 });
