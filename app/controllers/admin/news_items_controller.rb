c359b6de76bbb4ceade3f61434bdc596cae8789f
kustosz <marcin.kostrzewa@llp.pl>
2014-02-10 12:51:49 +0100

    fix news items admin controller after removing news references

--- app/controllers/admin/news_items_controller.rb
+++ app/controllers/admin/news_items_controller.rb
@@ -1,18 +1,11 @@
 class Admin::NewsItemsController < Admin::PageResourcesController
 
   before_action :set_news_list, :only => [:edit, :new, :create, :update]
-  before_action :set_references, :only => [:edit, :new, :create, :update]
 
   private
 
   def save_resources
-    if @page.save
-      NewsReference.where(news_id: @page.id).destroy_all
-      related_news_ids.each do |id|
-        NewsReference.create(news_id: @page.id, related_news_id: id)
-      end
-      true
-    end
+    @page.save
   end
 
   def site_identifier
@@ -27,11 +20,4 @@ class Admin::NewsItemsController < Admin::PageResourcesController
     @news_list = Cms::Site.find_by_identifier('news').pages.root.children.published
   end
 
-  def set_references
-    @references = NewsReference.where(news_id: @page.id)
-  end
-
-  def related_news_ids
-    [params[:news_item]['0'][:id], params[:news_item]['1'][:id]]
-  end
 end
