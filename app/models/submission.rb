51ed74b1e13ddfb48a1b1e2e44010467f80c240a
kustosz <marcin.kostrzewa@llp.pl>
2014-02-11 17:26:54 +0100

    add validations for job application form

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -22,6 +22,10 @@ class Submission < ActiveRecord::Base
 
   validates :form, presence: true
   validates :email, presence: true, email: true
+  validates :name, presence: true, if: :is_submitted
+  validates :phone, presence: true, if: :is_submitted
+  validates :cv, presence: true, if: :is_submitted
+  validates :cover_letter, presence: true, if: :is_submitted
 
   before_create :set_slug
 
