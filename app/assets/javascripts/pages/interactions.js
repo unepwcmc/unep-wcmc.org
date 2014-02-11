17ef72591e4056946d68f3449989332999f8a39e
Blanca Tortajada <tortajadablanca@gmail.com>
2014-02-11 19:20:51 +0000

    styling for error and confirmation messages

--- app/assets/javascripts/pages/interactions.js
+++ app/assets/javascripts/pages/interactions.js
@@ -13,15 +13,15 @@ $(document).ready(function(){
     $('button#fields-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"})
   });
 
-  $('div.container.status#ok').slideDown('normal');
-  $('div.container.status#warning').slideDown('normal');
+  $('div.status#ok').slideDown('normal');
+  $('div.status#warning').slideDown('normal');
 
-  $('div.container.status#ok i.icon-chevron-up').click(function() {
-    $('div.container.status#ok').slideUp('normal');
+  $('div.status#ok i.icon-chevron-up').click(function() {
+    $('div.status#ok').slideUp('normal');
   });
 
-  $('div.container.status#warning i.icon-chevron-up').click(function() {
-    $('div.container.status#warning').slideUp('normal');
+  $('div.status#warning i.icon-chevron-up').click(function() {
+    $('div.status#warning').slideUp('normal');
   });
 
   $('ul.subnav-small li.header#strengthen-button i').click(function() {
