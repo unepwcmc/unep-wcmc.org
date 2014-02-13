9103032eb7314d7c18246cf608e4df6934d2dd76
deciob <decio.battaglia@gmail.com>
2014-02-13 17:10:48 +0000

    toggle on dashboard sections and start of ppe

--- app/assets/javascripts/pages/dashboard/stats.js
+++ app/assets/javascripts/pages/dashboard/stats.js
@@ -3,27 +3,25 @@ angular.module('stats').controller('StatsCtrl', [
   '$rootScope',
   '$sce',
 function ($scope, $rootScope, $sce) {
-
   $scope.visible = false;
   $rootScope.$on('stats.visible', function(evt, args) {
     $scope.visible = true;
   });
-
 }]);
+
 
 angular.module('stats').controller('StatsBtnCtrl', [
   '$scope',
   '$rootScope',
   '$sce',
 function ($scope, $rootScope, $sce) {
-
   $scope.visible = true;
   $scope.showStats = function () {
     $scope.visible = false;
     $rootScope.$emit('stats.visible', true);
   }
-
 }]);
+
 
 angular.module('stats').controller('CountryPickerCtrl', [
   '$scope',
@@ -50,6 +48,7 @@ function ($scope, $rootScope, $sce, fetchData, GEO_ENTITIES_URL) {
 
 }]);
 
+
 angular.module('stats').controller('SapiStatsCtrl', [
   '$scope',
   '$rootScope',
@@ -61,12 +60,58 @@ function ($scope, $rootScope, $sce, fetchData, SAPI_API_URL, country) {
 
   $scope.loading = true;
   $scope.loaded = false;
+  // Trade
+  $scope.trade_export = true;
+  $scope.trade_import = false;
+  $scope.trade_title_export = 'Most exported species';
+  $scope.trade_title_import = 'Most imported species';
+  $scope.trade_title = $scope.trade_title_export;
+  $scope.trade_selector_export = 'See exports';
+  $scope.trade_selector_import = 'See imports';
+  $scope.trade_selector = $scope.trade_selector_import;
+  // Species
+  $scope.species_cites_eu = true;
+  $scope.species_cms = false;
+  $scope.species_title_cites_eu = 'Top cites eu listings';
+  $scope.species_title_cms = 'Top cms listings';
+  $scope.species_title = $scope.species_title_cites_eu;
+  $scope.species_selector_cites_eu = 'See cites eu';
+  $scope.species_selector_cms = 'See cms';
+  $scope.species_selector = $scope.species_selector_cms;  
   
   $rootScope.$on('geo.current_country', function(evt, args) {
     $scope.loading = true;
     $scope.loaded = false;
     getPromisedData(args);
   });
+
+  $scope.toggleTrade = function () {
+    if ($scope.trade_export) {
+      $scope.trade_export = false;
+      $scope.trade_import = true;
+      $scope.trade_title = $scope.trade_title_import;
+      $scope.trade_selector = $scope.trade_selector_export;
+    } else {
+      $scope.trade_export = true;
+      $scope.trade_import = false;
+      $scope.trade_title = $scope.trade_title_export;
+      $scope.trade_selector = $scope.trade_selector_import;
+    }
+  }
+
+  $scope.toggleSpecies = function () {
+    if ($scope.species_cites_eu) {
+      $scope.species_cites_eu = false;
+      $scope.species_cms = true;
+      $scope.species_title = $scope.species_title_cms;
+      $scope.species_selector = $scope.species_selector_cites_eu;
+    } else {
+      $scope.species_cites_eu = true;
+      $scope.species_cms = false;
+      $scope.species_title = $scope.species_title_cites_eu;
+      $scope.species_selector = $scope.species_selector_cms;  
+    }
+  }
 
   // Aggregates the least numerous groups.
   function getOtherSpeciesResults (sorted_results, top) {
@@ -106,17 +151,59 @@ function ($scope, $rootScope, $sce, fetchData, SAPI_API_URL, country) {
   var getPromisedData = function (current_country) {
     current_country = (typeof current_country === "undefined") ? country : current_country;
     return $.when(
-      fetchData(SAPI_API_URL + current_country, {kingdom: 'Animalia'})
+      fetchData(SAPI_API_URL + current_country, {data: {kingdom: 'Animalia'}})
     ).then(
       function (data) {
         var data = getTopSpeciesResults(data, 5).taxon_concept_stats;
-        $scope.species_cites_eu = data.species.cites_eu;
-        $scope.species_cms = data.species.cms;
-        $scope.trade_exports_top = data.trade.exports.top_traded;
-        $scope.trade_imports_top = data.trade.imports.top_traded;
+        $scope.species_cites_eu_data = data.species.cites_eu;
+        $scope.species_cms_data = data.species.cms;
+        $scope.trade_exports_top_data = data.trade.exports.top_traded;
+        $scope.trade_imports_top_data = data.trade.imports.top_traded;
         $scope.loading = false;
         $scope.loaded = true;
         $scope.$apply();
+      },
+      function (err) {console.log(err)}
+    );
+  }
+  getPromisedData();
+
+}]);
+
+
+angular.module('stats').controller('PpeStatsCtrl', [
+  '$scope',
+  '$rootScope',
+  '$sce', 
+  'fetchData', 
+  'PPE_API_URL',
+  'country',
+function ($scope, $rootScope, $sce, fetchData, PPE_API_URL, country) {
+
+  $scope.loading = true;
+  $scope.loaded = false;
+  
+  $rootScope.$on('geo.current_country', function(evt, args) {
+    $scope.loading = true;
+    $scope.loaded = false;
+    getPromisedData(args);
+  });
+
+  var getPromisedData = function (current_country) {
+    current_country = (typeof current_country === "undefined") ? country : current_country;
+    return $.when(
+      fetchData(PPE_API_URL, {data: {iso: current_country}, credentials: true})
+    ).then(
+      function (data) {
+        //var data = getTopSpeciesResults(data, 5).taxon_concept_stats;
+        //$scope.species_cites_eu = data.species.cites_eu;
+        //$scope.species_cms = data.species.cms;
+        //$scope.trade_exports_top = data.trade.exports.top_traded;
+        //$scope.trade_imports_top = data.trade.imports.top_traded;
+        //$scope.loading = false;
+        //$scope.loaded = true;
+        //$scope.$apply();
+        console.log(data);
       },
       function (err) {console.log(err)}
     );
--- app/assets/javascripts/pages/main.js
+++ app/assets/javascripts/pages/main.js
@@ -2,7 +2,7 @@ angular.module('ProjectsList', ['ngAnimate']);
 angular.module('DatasetsResources', []);
 
 angular.module('stats', ['ngAnimate', 'ui.select2'])
-  .constant('PPE_API_URL', 'http://www.protectedplanet.net/api2/countries')
+  .constant('PPE_API_URL', 'http://www.protectedplanet.net/api2/countries/')
   //.constant('SAPI_API_URL', 'http://sapi.unepwcmc-012.vm.brightbox.net/api/v1/stats/')
   .constant('SAPI_API_URL', 'http://localhost:3600/api/v1/stats/')
   .constant('GEO_ENTITIES_URL', 'http://sapi.unepwcmc-012.vm.brightbox.net/api/v1/geo_entities/')
@@ -14,7 +14,7 @@ angular.module('stats', ['ngAnimate', 'ui.select2'])
         type: 'GET',
         dataType: 'json',
         url: url,
-        data: params,
+        data: params.data,
         xhrFields: {
           withCredentials: params.credentials ? true : false
         }
