a5d2942b615c4f0389e676f39148765a553d0109
katszwn <katarzyna.szawan@lunarlogic.io>
2014-02-26 18:41:58 +0100

    Add helper methods to dataset field models

--- app/models/dataset_url_field.rb
+++ app/models/dataset_url_field.rb
@@ -1,2 +1,10 @@
 class DatasetUrlField < DatasetField
+
+  def self.for_dataset(dataset)
+    where(dataset_id: dataset.id)
+  end
+
+  def self.destroy_for_dataset(dataset)
+    for_dataset(dataset).destroy_all
+  end
 end
