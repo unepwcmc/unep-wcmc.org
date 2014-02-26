242864b83d0389e63c83d45b26a0582cd187fe9b
katszwn <katarzyna.szawan@lunarlogic.io>
2014-02-26 18:43:28 +0100

    Add dataset fields builder, update dataset controller

--- app/controllers/admin/datasets_controller.rb
+++ app/controllers/admin/datasets_controller.rb
@@ -1,9 +1,10 @@
 class Admin::DatasetsController < Admin::PageResourcesController
+  before_action :set_fields
 
   private
 
   def save_resources
-    @page.save
+    @page.save && fields.save
   end
 
   def site_identifier
@@ -12,6 +13,19 @@ class Admin::DatasetsController < Admin::PageResourcesController
 
   def layout_identifier
     'dataset'
+  end
+
+  def set_fields
+    @url_fields = url_fields_for_dataset
+    @file_fields = file_fields_for_dataset
+  end
+
+  def dataset_fields_params
+    params[:dataset_fields].try(:values) || []
+  end
+
+  def fields
+    DatasetFieldsBuilder.new(fields_params: dataset_fields_params, dataset: @page)
   end
 
 end
