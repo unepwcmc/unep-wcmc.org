b08ca3e83313bafa05b2350adb7717cfb83b0324
katszwn <katarzyna.szawan@lunarlogic.io>
2014-02-26 18:41:58 +0100

    Add helper methods to dataset field models

--- app/models/dataset_file_field.rb
+++ app/models/dataset_file_field.rb
@@ -1,3 +1,11 @@
 class DatasetFileField < DatasetField
   has_attached_file :file
+
+  def self.for_dataset(dataset)
+    where(dataset_id: dataset.id)
+  end
+
+  def self.destroy_for_dataset(dataset)
+    for_dataset(dataset).destroy_all
+  end
 end
