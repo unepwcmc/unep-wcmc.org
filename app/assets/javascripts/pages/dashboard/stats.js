902822f57cd5aea796786fb68ac1da90264e985b
Blanca Tortajada <tortajadablanca@gmail.com>
2014-03-04 17:37:57 +0000

    change to copy for consistency

--- app/assets/javascripts/pages/dashboard/stats.js
+++ app/assets/javascripts/pages/dashboard/stats.js
@@ -140,8 +140,8 @@ function ($scope, $resource, SAPI_API_URL, SAPI_SPECIES_GROUPS, countryService) 
   // Species
   $scope.sapi.species_cites = true;
   $scope.sapi.species_cms = false;
-  $scope.sapi.species_title_cites = 'Number of CITES-listed species in your country by group';
-  $scope.sapi.species_title_cms = 'Number of CMS-listed species in your country by group';
+  $scope.sapi.species_title_cites = 'Number of CITES-listed species by group';
+  $scope.sapi.species_title_cms = 'Number of CMS-listed species in by group';
   $scope.sapi.species_title = $scope.sapi.species_title_cites;
   $scope.sapi.species_selector_cites = 'See CITES';
   $scope.sapi.species_selector_cms = 'See CMS';
