ba04ec13b975a9c610325ec537be5ec96bed1c8e
deciob <decio.battaglia@gmail.com>
2014-04-08 11:31:55 +0100

    changed rake create table countries to create model

--- db/migrate/20140408103001_create_countries.rb
+++ db/migrate/20140408103001_create_countries.rb
@@ -3,6 +3,8 @@ class CreateCountries < ActiveRecord::Migration
     create_table :countries do |t|
       t.string :iso2
       t.string :name
+
+      t.timestamps
     end
   end
 end
---
+++ spec/factories/countries.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :country do
+    iso2 "MyString"
+    name "MyString"
+  end
+end
