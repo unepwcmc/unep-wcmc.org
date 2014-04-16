68dcb0d72b1696ce22cbb43b02fe9df24d6432e4
Julia Jacobs <julia@llp.pl>
2014-04-16 17:23:18 +0200

    Eager load cms block in featured projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def html_page_content(identifier, page)
+    return '' unless block = page.blocks.find {
+      |block| block.identifier == identifier.to_s
+    }
+    render :inline => ComfortableMexicanSofa::Tag.process_content(page, block.content)
+  end
+
   def block_content(identifier, page)
     return '' unless block = page.blocks.find {
       |block| block.identifier == identifier.to_s
@@ -11,5 +18,9 @@ module ApplicationHelper
       |block| block.identifier == identifier.to_s
     }
     block.files
+  end
+
+  def page_file(identifier, page)
+    page_files(identifier, page)[0]
   end
 end
