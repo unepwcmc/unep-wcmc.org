81d1765bef970cf192d3565ed52226b38d5ebb30
Julia Jacobs <julia@llp.pl>
2014-04-16 15:53:06 +0200

    Eager load cms blocks in datasets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def block_content(identifier, page)
+    return '' unless block = page.blocks.find {
+      |block| block.identifier == identifier.to_s
+    }
+    ComfortableMexicanSofa::Tag.process_content(page, block.content)
+  end
+
+  def page_files(identifier, page)
+    return nil unless block = page.blocks.find {
+      |block| block.identifier == identifier.to_s
+    }
+    block.files
+  end
 end
