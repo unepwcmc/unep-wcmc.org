46776b5284e4c3a50fb668054fd40c5888a13645
Julia Jacobs <julia@llp.pl>
2014-04-22 16:03:43 +0200

    Add subprogrammes

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -1,4 +1,5 @@
 class Programme < ActiveRecord::Base
   has_many :employments, dependent: :destroy
   has_many :employees, through: :employments, class_name: "Cms::Page"
+  has_many :subprogrammes, foreign_key: "parent_programme_id", class_name: "Programme"
 end
