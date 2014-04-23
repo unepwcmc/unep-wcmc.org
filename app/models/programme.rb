620c6b25fa0f5491d0b91830b1dae71e866bc553
Julia Jacobs <julia@llp.pl>
2014-04-23 16:33:42 +0200

    Validate parent programme not being a subprogramme

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -3,4 +3,14 @@ class Programme < ActiveRecord::Base
   has_many :employees, through: :employments, class_name: "Cms::Page"
   has_many :subprogrammes, foreign_key: "parent_programme_id", class_name: "Programme"
   accepts_nested_attributes_for :subprogrammes
+
+  validate :parent_programme_cant_be_a_subprogramme
+
+  private
+
+  def parent_programme_cant_be_a_subprogramme
+    if parent_programme_id && Programme.find(parent_programme_id).parent_programme_id
+      errors.add(:parent_programme_id, "is a subprogramme and can not be a parent programme")
+    end
+  end
 end
