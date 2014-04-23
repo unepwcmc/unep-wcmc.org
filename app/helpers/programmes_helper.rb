bde2607794a63807400a71a7f66412363133bdb6
Julia Jacobs <julia@llp.pl>
2014-04-23 17:23:46 +0200

    Eager load subprogrammes and employees for subprogrammes

--- app/helpers/programmes_helper.rb
+++ app/helpers/programmes_helper.rb
@@ -1,5 +1,8 @@
 module ProgrammesHelper
   def programmes
-    ::Programme.where(parent_programme_id: nil).includes(employees: [blocks: [:files]])
+    ::Programme.where(parent_programme_id: nil).includes(
+      employees: [blocks: [:files]],
+      subprogrammes:
+        [employees: [blocks: [:files]]])
   end
 end
