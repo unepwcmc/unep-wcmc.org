9c613ed59ae7f81e6a271f31abdff79a548bcae9
Julia Jacobs <julia@llp.pl>
2014-04-25 13:21:34 +0200

    Optimize images on upload

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module UnepWcmcOrg
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.active_record.observers = :"cms/page_observer"
-
+    config.active_record.observers = :"cms/page_observer", :"cms/file_observer"
   end
 end
