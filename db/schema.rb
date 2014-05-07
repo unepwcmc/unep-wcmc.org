fcb93096636b11910b6713264b2834c2014e9713
deciob <decio.battaglia@gmail.com>
2014-05-07 09:23:20 +0100

    committed schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430101507) do
+ActiveRecord::Schema.define(version: 20140506100829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -268,5 +268,17 @@ ActiveRecord::Schema.define(version: 20140430101507) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "vacancy_fields", force: true do |t|
+    t.integer  "vacancy_id"
+    t.string   "label"
+    t.string   "url"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
