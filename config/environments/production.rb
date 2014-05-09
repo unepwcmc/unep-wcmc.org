2ac88b3a2d8439d679b5db7b96623b49f994f906
deciob <decio.battaglia@gmail.com>
2014-05-09 11:48:48 +0100

    possible fix for invalid charachter in IE8 production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,10 @@ UnepWcmcOrg::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(
+    output: { ascii_only: true, quote_keys: true }
+  )
+  
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
