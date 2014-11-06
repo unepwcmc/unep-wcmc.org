86d83394e8889d9ba0ab23ee1ff3796f3499d17b
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-11-06 11:52:20 +0000

    Sort list of employees alphabetically

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -5,7 +5,9 @@ class Api::EmployeesController < ApplicationController
   end
 
   def index
-  	employees = Cms::Page.joins(:site).where('cms_sites.identifier' => 'employees').map{|e| e["label"]}
+    employees = Cms::Page.joins(:site).
+      where('cms_sites.identifier' => 'employees').
+      order('cms_pages.label ASC').map{|e| e["label"]}
   	render :json => employees
   end
 end
