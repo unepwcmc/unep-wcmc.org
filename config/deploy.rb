6f44d895f0e9faa320d92c68431290c4dd9416c4
strtwtsn <stuart.watson@unep-wcmc.org>
2015-07-29 15:33:14 +0100

    Fixed incorrect application name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'unepwcmc-cap4'
+set :application, 'unepwcmc'
 set :repo_url, 'git@github.com:unepwcmc/unep-wcmc.org'
 
 # Default branch is :master
