59d8452558870100f3406ca81a1f55d44de3239e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-09-04 16:21:22 +0100

    Revert "Consistent sorting"
    This reverts commit fc6b31f77a0951b9aa406b7790d325aa352ec7c6.

--- app/assets/javascripts/pages/datasets_resources/datasets_ctrl.js
+++ app/assets/javascripts/pages/datasets_resources/datasets_ctrl.js
@@ -92,14 +92,9 @@ function ($scope, $sce) {
         $scope.activeDatasets.splice(0, 0, $scope.activeDatasets.splice(from, 1)[0]);
       }
     } else if ($scope.sortOrder === 0) {
-      $scope.activeDatasets = _($scope.activeDatasets).chain()
-      .sortBy(function(dataset) {
-        return dataset.title;
-      })
-      .reverse()
-      .sortBy(function(dataset) {
+      $scope.activeDatasets = _.sortBy($scope.activeDatasets, function(dataset) {
         return dataset.publication_date_year_epoch;
-      }).reverse().value();
+      }).reverse();
     } else if ($scope.sortOrder === 1) {
       $scope.activeDatasets = _.sortBy($scope.activeDatasets, "title");
     }
