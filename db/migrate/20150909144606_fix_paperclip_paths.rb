2cc6623239c0d734beff82fda8d5c096dbb2740a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-09-10 12:25:23 +0100

    fixed image paths within admin tool

--- db/migrate/20150909144606_fix_paperclip_paths.rb
+++ db/migrate/20150909144606_fix_paperclip_paths.rb
@@ -6,7 +6,10 @@ class FixPaperclipPaths < ActiveRecord::Migration
       FileUtils.mv(src_dir, target_dir)
     end
     Comfy::Cms::Block.where("content LIKE '%system/cms%'").each do |b|
-      b.update_attribute(:content, b.content.gsub(/system\/cms/, 'system/comfy/cms'))
+      b.update_attribute(:content, b.content.gsub('system/cms', 'system/comfy/cms'))
+    end
+    Comfy::Cms::Block.where("content LIKE '%../../../system%'").each do |b|
+      b.update_attribute(:content, b.content.gsub('../../../system', '/system'))
     end
     execute "UPDATE comfy_cms_pages SET content_cache = NULL"
   end
