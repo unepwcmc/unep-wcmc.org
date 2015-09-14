696885d12091317d40cd74b962724aae9c63caef
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-09-14 11:20:51 +0100

    Fixes #102645166 by setting default response format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  BOGUS_REQUEST_FORMATS = ['*/*;', '/;', 'hc/url;*/*']
+  DEFAULT_RESPONSE_FORMAT = :html
+  before_filter :set_default_response_format
+
+  def set_default_response_format
+    request.format = DEFAULT_RESPONSE_FORMAT if request.format.in? BOGUS_REQUEST_FORMATS
+  end
 
   def after_sign_in_path_for(resource)
     comfy_admin_cms_path
