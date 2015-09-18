fc943ac5bd8bc5d9489fb3cac5e92c7549b39dd6
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-09-18 10:33:28 +0100

    hot fix for employees API

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -6,8 +6,8 @@ class Api::EmployeesController < ApplicationController
 
   def index
     employees = Comfy::Cms::Page.joins(:site).
-      where('cms_sites.identifier' => 'employees').
-      order('cms_pages.label ASC').map{|e| e["label"]}
+      where('comfy_cms_sites.identifier' => 'employees').
+      order('comfy_cms_pages.label ASC').map{|e| e["label"]}
   	render :json => employees
   end
 end
