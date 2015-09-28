8736e34e9c315ec4d186106f6c144b62a5ea1895
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-09-28 14:42:21 +0100

    Added test for form not found

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -17,6 +17,11 @@ describe SubmissionsController do
       get :new, form_id: @form.id
       assigns(:submission).field_submissions.size.should == 2
     end
+
+    it "should render 404 when form not found" do
+      get :new, form_id: 'nonsense'
+      expect(response.status).to eq(404)
+    end
   end
 
   describe "#edit" do
