4938c143bcae5cbb2e819cde358a737da0559cc2
Andrew Potter <hello@drewpotter.com>
2018-03-09 14:19:34 +0000

    Basic zip working for single job application

--- lib/download/generate_zip.rb
+++ lib/download/generate_zip.rb
@@ -1,19 +1,25 @@
 class Download::GenerateZip
 
-  def initialize
+  def initialize zip_path
     puts "initialize generate zip with the path of the zip path"
+    @zip_path = zip_path
   end
 
-  def add_file_to_zip(file_path, zipped_file_path)
-    puts "adds file to zip (updates it if present) and creates zip if not already present"
+  def add_to_zip resource_path
+    puts "add file or directory to zip and creates zip if not already present"
+    system("zip -jru #{@zip_path} #{resource_path}")
   end
 
-  def application_generate_zip(form_id)
-    puts "generate zip for single application and add manual attachment to submission model"
+  def application_generate_zip submission_id
+    puts "generate zip for single job application"
+    submission = Submission.find_by(id: submission_id)
+    add_to_zip(submission.cv.path)
+    add_to_zip(submission.application_form.path)
+    add_to_zip(submission.cover_letter.path)
   end
 
-  def all_applications_generate_zip(vacancy_id)
-    puts "generate zip for all applications and add manual attachment to form model"
+  def all_applications_generate_zip form_id
+    puts "generate zip for all applications"
   end
 
 end
