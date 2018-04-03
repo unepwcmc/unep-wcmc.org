1c3cba2a13e82eded2c80171fb9e383b48ff5ae9
Andrew Potter <hello@drewpotter.com>
2018-04-03 13:44:02 +0100

    Fixed file paths for combined zip for all job application downloads

--- lib/download/generate_zip.rb
+++ lib/download/generate_zip.rb
@@ -28,12 +28,13 @@ class Download::GenerateZip
     form.submissions.where(is_submitted: true).each do |submission|
       candidate_path = "#{submission.name}".parameterize.underscore
       all_submissions_path = "all_submissions"
+      vacancy_label = form.vacancy.label.scan(/\(([^)]+)\)/).first
 
       system("mkdir #{zipped_files_path}/#{candidate_path}", chdir: @path)
 
-      system("cp #{submission.cv.path} #{zipped_files_path}/#{candidate_path}/#{submission.name.parameterize.underscore}_#{submission.cv_file_name}", chdir: @path)
-      system("cp #{submission.application_form.path} #{zipped_files_path}/#{candidate_path}/#{submission.name.parameterize.underscore}_#{submission.application_form_file_name}", chdir: @path)
-      system("cp #{submission.cover_letter.path} #{zipped_files_path}/#{candidate_path}/#{submission.name.parameterize.underscore}_#{submission.cover_letter_file_name}", chdir: @path)
+      system("cp #{submission.cv.path} #{zipped_files_path}/#{candidate_path}/#{vacancy_label}_#{candidate_path}_CV#{File.extname(submission.cv_file_name)}", chdir: @path)
+      system("cp #{submission.application_form.path} #{zipped_files_path}/#{candidate_path}/#{vacancy_label}_#{candidate_path}_Application#{File.extname(submission.application_form_file_name)}", chdir: @path)
+      system("cp #{submission.cover_letter.path} #{zipped_files_path}/#{candidate_path}/#{vacancy_label}_#{candidate_path}_Cover_letter#{File.extname(submission.cover_letter_file_name)}", chdir: @path)
 
       system("mkdir #{zipped_files_path}/#{all_submissions_path}", chdir: @path)
       system("cp #{zipped_files_path}/#{candidate_path}/* #{zipped_files_path}/#{all_submissions_path}", chdir: @path)
