ab824fa450c07b4b3351ae4a87a4c4481b13c939
Andrew Potter <hello@drewpotter.com>
2018-07-24 10:40:00 +0100

    Adjust the where query to include less than or equal to current the date

--- config/initializers/overrides.rb
+++ config/initializers/overrides.rb
@@ -1,3 +1,3 @@
 Comfy::Cms::Page.class_eval do
-  scope :published, -> { unscoped.where('published_date < ? AND is_published = true', Date.today) }
+  scope :published, -> { unscoped.where('published_date <= ? AND is_published = true', Date.today) }
 end
