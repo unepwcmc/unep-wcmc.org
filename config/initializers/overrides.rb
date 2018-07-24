59a30ba174fad3fdc759db6f3b2a55533fd110cb
Andrew Potter <hello@drewpotter.com>
2018-07-24 15:48:19 +0100

    Extract published date from the time attribute on the submitted page

--- config/initializers/overrides.rb
+++ config/initializers/overrides.rb
@@ -1,3 +1,16 @@
 Comfy::Cms::Page.class_eval do
+  before_save :extract_published_date
+
   scope :published, -> { unscoped.where('published_date <= ? AND is_published = true', Date.today) }
+
+  private
+
+  def extract_published_date
+    self.blocks_attributes.map do |attr|
+      if attr[:identifier] == "time"
+        self.published_date = attr[:content]
+        return
+      end
+    end
+  end
 end
