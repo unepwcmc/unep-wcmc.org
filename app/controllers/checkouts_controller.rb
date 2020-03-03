33eff3af9ec088ce83693d2d8db6ed1b57dee94e
lucacug <luca.cugliari@unep-wcmc.org>
2020-03-03 16:08:38 +0000

    add status and body to the response

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,16 +1,13 @@
 class CheckoutsController < ApplicationController
-  def create
-    # render json: PaymentIntent.create_payment_intent params[:amount]
-    @response = create_session params['amount']
 
-    render json: @response
+  def create
+    render create_session(params['amount'])
   end
 
   def create_session amount
     # Set your secret key. Remember to switch to your live secret key in production!
     # See your keys here: https://dashboard.stripe.com/account/apikeys
     Stripe.api_key = 'sk_test_bEYLlmqQa77A6QNdxHURFADg00q55Dcpr3'
-
     session = Stripe::Checkout::Session.create(
       submit_type: 'donate',
       payment_method_types: ['card'],
@@ -19,12 +16,13 @@ class CheckoutsController < ApplicationController
         description: 'Donation to UNEP-WCMC',
         amount: amount,
         currency: 'gbp',
-        quantity: 1,
+        quantity: 1
       }],
       success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: 'https://google.co.uk'
     )
+    return { body: session, status: '200' }
   rescue Stripe::InvalidRequestError => e
-    e.as_json
+    return { body: e.json_body, status: e.http_status }
   end
 end
