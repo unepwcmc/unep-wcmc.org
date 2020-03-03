b2b8fcca98a48aa39e0f4ec528b135f4ba819ae6
lucacug <luca.cugliari@unep-wcmc.org>
2020-03-03 17:53:36 +0000

    add stripe public key to the response

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -7,7 +7,6 @@ class CheckoutsController < ApplicationController
   def create_session amount
     # Set your secret key. Remember to switch to your live secret key in production!
     # See your keys here: https://dashboard.stripe.com/account/apikeys
-    Stripe.api_key = 'sk_test_bEYLlmqQa77A6QNdxHURFADg00q55Dcpr3'
     session = Stripe::Checkout::Session.create(
       submit_type: 'donate',
       payment_method_types: ['card'],
@@ -21,7 +20,7 @@ class CheckoutsController < ApplicationController
       success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: 'https://google.co.uk'
     )
-    { body: session, status: '200' }
+    { body: session, status: '200', stripe_key: Rails.application.secrets.stripe_public_key }
   rescue Stripe::InvalidRequestError => e
     { body: e.json_body, status: e.http_status }
   end
