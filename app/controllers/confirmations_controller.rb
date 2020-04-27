26fe1e1cbb504eb2aec9d2422264db76fb12113b
Stanley Liu <sliu1993@hotmail.co.uk>
2020-04-27 15:24:33 +0100

    fixed broken logic for error handling with another Devise method

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -18,31 +18,32 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def set_user
-    if params[:confirmation_token].blank?
-      flash[:notice] = 'Missing confirmation token. Please contact your administrator to resend confirmation instructions.'
-      redirect_to '/'
-      return
-    end
+    pre_checks
     @user = User.confirm_by_token(params[:confirmation_token])
-    # Handling errors if confirmation_token is valid
-    post_checks
   end
 
   def password_params
     params.require(:user).permit(:password, :password_confirmation)
   end
 
-  def post_checks
-    if @user
-      if !@user.confirmed_at.nil?
-        flash[:notice] = "Email address already confirmed, please sign in."
-        sign_out current_user
-        # To stop users using multiple confirmation links if signed in with one account already.
-        redirect_to new_user_session_path
-      elsif @user.id.nil?
+  def pre_checks
+    if params[:confirmation_token].blank?
+      flash[:notice] = 'Missing confirmation token. Please contact your administrator to resend confirmation instructions.'
+      redirect_to '/'
+      return
+    end
+    @found_user = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
+    if @found_user
+      if @found_user.id.nil?
         flash[:notice] = 'Invalid confirmation token. Please contact your administrator to resend confirmation instructions.'
         redirect_to '/'
+      elsif !@found_user.confirmed_at.nil?
+        sign_out current_user
+        # To stop users using multiple confirmation links if signed in with one account already.
+        flash[:notice] = "Email address already confirmed, please sign in."
+        redirect_to new_user_session_path
       end
     end
+    # raise
   end
 end
