2d6f65c3682cc612f5dcb8fa412387ad5bbe21eb
Stanley Liu <sliu1993@hotmail.co.uk>
2020-04-28 12:09:38 +0100

    refactored code to handle already signed-in users as well as removing checks for setting new password

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   skip_before_filter :authenticate_user!
-  before_action :set_user, only: [:show, :update]
+  before_action :pre_checks, only: :show
+  before_action :set_user_update, only: :update
 
   def update
     if @user.update(password_params)
@@ -17,8 +18,8 @@ class ConfirmationsController < Devise::ConfirmationsController
 
   private
 
-  def set_user
-    pre_checks
+  def set_user_update
+    # No checks as user must pass confirmation to see the show page - and ergo to update their password
     @user = User.confirm_by_token(params[:confirmation_token])
   end
 
@@ -27,21 +28,24 @@ class ConfirmationsController < Devise::ConfirmationsController
   end
 
   def pre_checks
-    if params[:confirmation_token].blank?
-      flash[:error] = 'Missing confirmation token. Please contact your administrator to resend confirmation instructions.'
-      redirect_to '/'
-      return
-    end
     @found_user = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
     if @found_user
       if @found_user.id.nil?
-        flash[:error] = 'Invalid confirmation token. Please contact your administrator to resend confirmation instructions.'
+        flash[:error] = 'Invalid or already confirmed confirmation token. Please contact your administrator to resend confirmation instructions.'
         redirect_to '/'
-      elsif !@found_user.confirmed_at.nil?
-        sign_out current_user
-        # To stop users using multiple confirmation links if signed in with one account already.
-        flash[:error] = "Email address already confirmed, please sign in."
-        redirect_to new_user_session_path
+        return
+      elsif !@found_user.id.nil?
+        if @found_user != current_user && !@found_user.confirmed_at.nil?
+          sign_out current_user
+          # To stop users using multiple confirmation links if signed in with one account already.
+          flash[:notice] = "Account already confirmed. Please sign in (again if you have been logged out)."
+          redirect_to new_user_session_path
+          return
+        elsif @found_user != current_user && @found_user.confirmed_at.nil?
+          sign_out current_user
+          flash[:notice] = "Signed you out. Please confirm your new account."
+          @user = User.confirm_by_token(params[:confirmation_token])
+        end
       end
     end
   end
