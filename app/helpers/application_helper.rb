37812d7a2d049be94a0f95e232d85db590387b72
nym1k <ross.king@unep-wcmc.org>
2020-05-06 09:17:59 +0100

    Generating email and twitter share links using application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,4 +40,63 @@ module ApplicationHelper
 
     link_to(text, path, opts)
   end
+
+  def get_current_page_full_url
+    return request.base_url + request.fullpath
+  end
+
+  def email_share_subject
+    email_subject = ''
+
+    if cms_block_content(:email_share_subject) != ''
+      email_subject = cms_block_content(:email_share_subject)
+    elsif cms_block_content(:project_name, @cms_page) != ''
+      email_subject = cms_block_content(:project_name, @cms_page)
+    elsif cms_block_content(:news_header, @cms_page) != ''
+      email_subject = cms_block_content(:news_header, @cms_page)
+    elsif cms_block_content(:position_title, @cms_page) != ''
+      email_subject = cms_block_content(:position_title, @cms_page) + ' at UNEP-WCMC'
+    else
+      email_subject = 'Visit the UNEP-WCMC website'
+    end
+
+    return ERB::Util.url_encode(email_subject)
+  end
+
+  def email_share_body
+    email_body = ''
+
+    if cms_block_content(:email_share_content) != ''
+      email_body = cms_block_content(:email_share_content)
+    elsif cms_block_content(:project_name, @cms_page) != ''
+      email_body = cms_block_content(:project_name, @cms_page) + ': ' + get_current_page_full_url
+    elsif cms_block_content(:news_header, @cms_page) != ''
+      email_body = cms_block_content(:news_header, @cms_page) + ': ' + get_current_page_full_url
+    elsif cms_block_content(:position_title, @cms_page) != ''
+      email_body = cms_block_content(:position_title, @cms_page) + ' at UNEP-WCMC: ' + get_current_page_full_url
+    else
+      email_body = 'Visit the UNEP-WCMC website: ' + get_current_page_full_url
+    end
+
+    return ERB::Util.url_encode(email_body)
+  end
+
+  def twitter_share_text
+    share_text = ''
+    share_text_end = ' | Read more on the @unepwcmc website: ' + get_current_page_full_url
+
+    if cms_block_content(:email_share_subject) != ''
+      share_text = cms_block_content(:email_share_subject) + share_text_end
+    elsif cms_block_content(:project_name, @cms_page) != ''
+      share_text = cms_block_content(:project_name, @cms_page) + share_text_end
+    elsif cms_block_content(:news_header, @cms_page) != ''
+      share_text = cms_block_content(:news_header, @cms_page) + share_text_end
+    elsif cms_block_content(:position_title, @cms_page) != ''
+      share_text = cms_block_content(:position_title, @cms_page) + share_text_end
+    else
+      share_text = 'Visit the @unepwcmc website | ' + get_current_page_full_url
+    end
+
+    return ERB::Util.url_encode(share_text)
+  end
 end
