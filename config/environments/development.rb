979f354b8e750188266eae2b6aa9d40a126dbf2d
Stanley Liu <sliu1993@hotmail.co.uk>
2020-06-29 19:13:37 +0100

    added in mailer secrets to development, and also added in mailer settings for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
+secrets = Rails.application.secrets
 UnepWcmcOrg::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,9 +14,7 @@ UnepWcmcOrg::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -29,7 +27,20 @@ UnepWcmcOrg::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  config.action_mailer.delivery_method = :smtp #for the mac apparently run::sudo postfix start
+  config.action_mailer.asset_host = secrets['mailer']['asset_host']
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', :protocol => 'http' }
 
+  
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => secrets['mailer']['address'],
+    :port => secrets['mailer']['port'],
+    :domain => secrets['mailer']['domain'],
+    :authentication => :login,
+    :user_name => secrets['mailer']['username'],
+    :password => secrets['mailer']['password']
+  }
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
+secrets = Rails.application.secrets
 UnepWcmcOrg::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  secrets = Rails.application.secrets
+  
 
   # Code is not reloaded between requests.
   config.cache_classes = true
