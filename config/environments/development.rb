da45cd0a891b222a0ab0087d9f2bed0ec07e2276
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-07 11:22:07 +0100

    added back letter opener settings in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ UnepWcmcOrg::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
+
   config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
@@ -32,7 +32,7 @@ UnepWcmcOrg::Application.configure do
 
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000', :protocol => 'http' }
-
+  config.action_mailer.perform_deliveries = true
 
 
 end
